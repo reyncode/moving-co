1df74f0101c328c631fab0d9389b1ba83fab98ab
ViaAR <hellport@protonmail.com>
2021-08-03 13:44:16 -0300

    svg issue fixed

--- src/components/InfoSection/data.js
+++ src/components/InfoSection/data.js
@@ -8,7 +8,7 @@ export const homeObjOne = {
     description: 'Founded in 2021 by two friends, we help you co-ordinate your move so you have access to the resources you need ',
     buttonLabel: 'Get Started',
     imgStart: false,
-    img: require('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg').default,
     alt: 'standing',
     dark: true,
     primary: true,
