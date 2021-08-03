ea009b2d1d508fbbe6a9b9262c2b699c56af470c
ViaAR <hellport@protonmail.com>
2021-08-03 12:25:46 -0300

    working on InfoSection data

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Change This',
+    headline: 'Depends on which section',
+    description: 'get information from this sections and click on contact',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+
+}
