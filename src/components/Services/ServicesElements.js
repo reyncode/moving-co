b10108b20aa379f6bf6272a96941b1e215e16de6
ViaAR <hellport@protonmail.com>
2021-08-05 14:05:36 -0300

    separated short and long haul cards

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -20,15 +20,10 @@ export const ServicesWrapper = styled.div`
     max-width: 1000px;
     margin: 0 auto;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
     align-items: center;
     grid-gap: 16px;
     padding: 0 50px;
-
-
-    @media screen and (max-width: 1000px) {
-        grid-template-columns: 1fr 1fr;
-    }
 
     @media screen and (max-width: 768px) {
         grid-template-columns: 1fr;
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -11,26 +11,32 @@ import {
 import Icon1 from '../../images/svg-2.svg'
 import Icon2 from '../../images/svg-3.svg'
 import Icon3 from '../../images/svg-4.svg'
+import Icon4 from '../../images/svg-5.svg'
 
 const Services = () => {
     return (
         <ServicesContainer id='services'>
-            <ServicesH1>Services</ServicesH1>
+            <ServicesH1>Solutions</ServicesH1>
             <ServicesWrapper>
                 <ServicesCard>
                     <ServicesIcon src={Icon2} />
                     <ServicesH2>Express</ServicesH2>
-                    <ServicesP>Same-day transportation for orders booked at least 4 hours prior to the pickup time, and next-day transportation for orders booked anytime the day prior.</ServicesP>
+                    <ServicesP>Same-day transportation for orders booked at least 4 hours prior to the pickup time.</ServicesP>
                 </ServicesCard>
                 <ServicesCard>
                     <ServicesIcon src={Icon1} />
                     <ServicesH2>Scheduled</ServicesH2>
-                    <ServicesP>Transportation that can be booked up to 30 days in advance.</ServicesP>
+                    <ServicesP>Scheduled transportation that can be booked up to 30 days in advance.</ServicesP>
                 </ServicesCard>
                 <ServicesCard>
                     <ServicesIcon src={Icon3} />
-                    <ServicesH2>Short- and long-haul</ServicesH2>
-                    <ServicesP>Short-haul transportation within the HRM, and up to 150 km outside in either direction. Long-haul transportation is province-wide, exceeding 150km. </ServicesP>
+                    <ServicesH2>Short-haul</ServicesH2>
+                    <ServicesP>Short-haul transportation within the HRM, and up to 150 km outside in either direction.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon4} />
+                    <ServicesH2>Long-haul</ServicesH2>
+                    <ServicesP>Long-haul transportation is province-wide, exceeding 150km. </ServicesP>
                 </ServicesCard>
             </ServicesWrapper>
         </ServicesContainer>
