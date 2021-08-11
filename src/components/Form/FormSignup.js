7af8551d7e41edc02034653c0f29697ad32d14d2
ViaAR <hellport@protonmail.com>
2021-08-11 10:46:31 -0300

    adding phone number to contact

--- src/components/Form/FormSignup.js
+++ src/components/Form/FormSignup.js
@@ -13,8 +13,8 @@ const FormSignup = ({ submitForm }) => {
     return (
         <div className='form-content-right'>
             <form onSubmit={handleSubmit} className='form' noValidate>
-                <h1>
-                    Send us a message and we will get back to you as soon as possible!
+                <h1 className='contact-header'>
+                    Send us a message below or call 902-221-8615!
                 </h1>
                 <div className='form-inputs'>
                     <label className='form-label'>Full Name</label>
