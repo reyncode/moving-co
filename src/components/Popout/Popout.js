333369f549179b66d89bbadf01525cf0dfaf97e6
ViaAR <hellport@protonmail.com>
2021-08-16 14:48:23 -0300

    version updated

--- src/components/Popout/Popout.js
+++ src/components/Popout/Popout.js
@@ -1,17 +1,39 @@
 import React from 'react'
-import { PopoutContainer, PopoutWrapper, BtnWrap } from './PopoutElements'
+import {
+    PopoutContainer,
+    PopoutWrapper,
+    BtnWrap,
+    ContactInfo,
+    Column1,
+    Column2,
+    EmailIcon,
+    PhoneIcon,
+    FacebookIcon
+} from './PopoutElements'
 import { Button } from '../ButtonElements'
+
 
 function Popout(props) {
     return (props.trigger) ? (
         <PopoutContainer>
             <PopoutWrapper>
+                <ContactInfo>
+                    <Column1>
+                        <EmailIcon />
+                        <PhoneIcon />
+                        <FacebookIcon />
+                    </Column1>
+                    <Column2>
+                        <h2>conveniencetransportationhfx@gmail.com</h2>
+                        <h2>(902) 221-8615</h2>
+                        <h1>Convenience Transportation HFX</h1>
+                    </Column2>
+                </ContactInfo>
                 <BtnWrap>
                     <Button onClick={() => props.setTrigger(false)}
                     >Close</Button>
                     {props.children}
                 </BtnWrap>
-
             </PopoutWrapper>
         </PopoutContainer>
     ) : "";
--- src/components/Popout/PopoutElements.js
+++ src/components/Popout/PopoutElements.js
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsPhone } from 'react-icons/bs';
+import { RiFacebookCircleLine } from 'react-icons/ri';
+
 
 export const PopoutContainer = styled.div`
     position: fixed;
@@ -21,10 +25,32 @@ export const PopoutWrapper = styled.div`
     width: 100%;
     height: 100%;
     max-width: 640px;
-    max-height: 600px;
+    max-height: 320px;
     background-color: #fff;
     border-radius: 10px;
     box-shadow: 0 12px 15px rgba(0,0,0,0.3);
+`
+
+export const ContactInfo = styled.div`
+    display: grid;
+    // grid-auto-columns: minmax(auto, 1fr);
+    align-items: center;
+    grid-template-areas: 'col1 col2';
+
+    border-radius: 10px;
+    background: #010606;
+`
+
+export const Column1 = styled.div`
+    margin-bottom: 15px;
+    padding: 0 15px;
+    // grid-area: col3;
+`
+
+export const Column2 = styled.div`
+    margin-bottom: 15px;
+    padding: 0 15px;
+    // grid-area: col1;
 `
 
 export const BtnWrap = styled.div`
@@ -37,4 +63,24 @@ export const BtnWrap = styled.div`
     @media screen and (max-width: 480px) {
         
     }
+`
+export const EmailIcon = styled(HiOutlineMail)`
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-size: 55px;
+    color: #fff;
+`
+
+export const PhoneIcon = styled(BsPhone)`
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-size: 55px;
+    color: #fff;
+`
+
+export const FacebookIcon = styled(RiFacebookCircleLine)`
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-size: 55px;
+    color: #fff;
 `
