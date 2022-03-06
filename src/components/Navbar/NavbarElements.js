3a87de6db6f4225f32c0a19191331b1c3ba1c7a4
apollo <apollo@DESKTOP-UPD06S5>
2022-03-06 15:21:22 -0400

    Removed dupe colon

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -27,7 +27,7 @@ export const NavbarContainer = styled.div`
     z-index: 1;
     width: 100%;
     padding: 0 15px;
-    max-width:: 1100px;
+    max-width: 1100px;
 `
 
 export const NavLogo = styled(LinkR)`
